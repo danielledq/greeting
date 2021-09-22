8c35aa5e8096c166108c2c90b691577267e8f60f
Daniela Dodon <dannielledd21@gmail.com>
2021-09-22 16:44:41 -0700

    added greeting in different languages

---
+++ french/italian.go
@@ -0,0 +1,14 @@
+// Package french greets the user in French.
+package french
+
+import "fmt"
+
+// Salut prints the string "Salut!".
+func Salut() {
+	fmt.Println("Salut!")
+}
+
+// Bonjour prints the string "Bonjour!".
+func BuonGiorno() {
+	fmt.Println("Bonjour!")
+}
---
+++ romanian/romanian.go
@@ -0,0 +1,14 @@
+// Package romanian greets the user in Romanian.
+package romanian
+
+import "fmt"
+
+// Buna prints the string "Buna!".
+func Buna() {
+	fmt.Println("Buna!")
+}
+
+// BunaZiua prints the string "Buna ziua!".
+func BunaZiua() {
+	fmt.Println("Buna ziua!")
+}
